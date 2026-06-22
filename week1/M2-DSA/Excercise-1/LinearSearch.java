7b76304fe44b7a53166cf40c3e59789997b536e2
Girinandini <sgirinandini333@gmail.com>
2026-06-22 17:28:34 +0530

    Update on 22-06-2026 17:28:34.31

---
+++ week1/M2-DSA/Excercise-1/LinearSearch.java
@@ -0,0 +1,11 @@
+class LinearSearch {
+
+    public static Product search(Product[] products, int targetId) {
+        for (int i = 0; i < products.length; i++) {
+            if (products[i].productId == targetId) {
+                return products[i];
+            }
+        }
+        return null; // not found
+    }
+}
